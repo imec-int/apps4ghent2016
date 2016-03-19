5c226c198a22ee1be46b3767ac3db07e2b32c55c
SamDecrock <sam.decrock@gmail.com>
2016-03-19 11:49:44 +0100

    basis webapp

---
+++ app/app.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+//app.use(favicon(__dirname + '/public/favicon.ico')); // uncomment after placing your favicon in /public
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, res) {
+	res.render('index', { title: 'Apps 4 Ghent 2016!' });
+});
+
+
+
+
+
+
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handler:
+app.use(function (err, req, res, next) {
+	if(!err.status) err.status = 500;
+
+	res.status(err.status);
+
+	if(err.status == 404)
+		return res.send(err.toString()); // 404 errors are not worth logging.
+
+	if (app.get('env') === 'production'){
+		console.log(err.stack); // log to console
+		return res.send("An error occured: " + err.status); // don't log to user
+	} else {
+		next(err); // log to console and user
+	}
+});
+
+
+
+var webserver = app.listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + webserver.address().port);
+});
+

5593b39bd9b0b21d6a21f2e72c3a0910b7caa332
SamDecrock <sam.decrock@gmail.com>
2016-03-19 12:02:45 +0100

    basic form building met backbone-forms

--- app/public/javascripts/index.js
+++ app/public/javascripts/index.js
@@ -2,7 +2,67 @@ var App = function (options){
 
 	var init = function (){
 		console.log("init");
+
+		buildForm();
 	};
+
+
+
+	var buildForm = function () {
+		// form bouwen met https://github.com/powmedia/backbone-forms
+		// zonder gebruik te maken van alle backbone-features:
+
+		var someForm = new Backbone.Form({
+			schema: {
+				naam: {
+					validators: ['required']
+				},
+				voornaam: {
+					validators: ['required']
+				},
+				geboortedatum: {
+					title: 'Geboortedatum',
+					type: 'Date',
+				},
+				adres: {
+					editorAttrs: {placeholder: "Straat + huisnummer (+ busnummer)"},
+					validators: ['required']
+				},
+				postcode: {
+					validators: ['required']
+				},
+				gemeente: {
+					validators: ['required']
+				},
+				land: {
+					type: 'Select',
+					options: countries
+				}
+			},
+			data: {
+				land: 'België'
+			},
+			submitButton: "Bereken voor mij de ultieme vrijgezellenroute!"
+		});
+
+		$('.formcontainer').empty().append(someForm.render().el);
+
+
+		someForm.on('submit', function (event) {
+			event.preventDefault(); // nie posten!
+
+			var errors = someForm.validate();
+			if(errors) return;
+
+
+			var data = someForm.getValue();
+			data.geboortedatum = data.geboortedatum.getTime();
+			$.put('/rest/sendata', data, function (data) {
+				if(data.err) { return console.log(data.err); }
+				console.log('data successfully posted to server!')
+			});
+		});
+	}
 
 	return {
 		init: init
